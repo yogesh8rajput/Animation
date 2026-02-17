c5d51bf5dcefe9a6cb2463ddbec5655d279634f4
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2026-02-17 19:42:45 +0530

    toggle

---
+++ New folder/toggle.js
@@ -0,0 +1,7 @@
+  const toggleBtn = document.getElementById("toggleBtn");
+  const navbar1 = document.getElementById("navbar1");
+
+  toggleBtn.addEventListener("click", function () {
+    navbar1.classList.toggle("active");
+  });
+
