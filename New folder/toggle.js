aa7beb0c8a03b764aa6ed2d80b4c09d2e8b21742
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2026-07-14 20:58:17 +0530

    Update toggle.js

--- New folder/toggle.js
+++ New folder/toggle.js
@@ -4,4 +4,4 @@
   toggleBtn.addEventListener("click", function () {
     navbar1.classList.toggle("active");
   });
-//this code to the toggle button work //toggle button only java script 
+//this code to the toggle button work //toggle button only java script and htmal css
